a4cea0e6cd9f0010460b50a44defaba10194628f
Dex <dexter.i.horthy@gmail.com>
2018-05-02 16:33:51 -0700

    ship ref docs

--- hack/docs/src/markdown.ts
+++ hack/docs/src/markdown.ts
@@ -1,39 +1,7 @@
 import * as fs from "fs";
+import * as _ from "lodash";
 import * as yaml from "js-yaml";
-
-const INDEX_DOC = `---
-categories:
-- ship-application-specs
-date: 2018-01-17T23:51:55Z
-description: Reference Documentation for defining your Ship application assets 
-index: docs
-title: Ship Assets
-weight: "1"
-gradient: "purpleToPink"
----
-
-## Ship Assets
-
-This is the reference documenation for Ship assets. To get started with Ship, head on over to [Getting Started with Ship](TODO).
-
-Assets are the core object that enables you to describe applications managed by Ship. They allow you to define scripts, manifests, and application artifacts needed to deploy your application to your end customer's internal infrastructure. The goal of Ship assets is to give your customers controlled, transparent access to the same resources you use to deploy your SaaS application to your own private cloud. Assets can include things like:
-
-- Scripts for installing and upgrading your application to a cloud server
-- Private Docker images or ${"`tar.gz`"} archives
-- Container orchestration manifests for Kubernetes or Docker Compose
-- Modules for infrastructure automation tools like Chef, Ansible, Salt, or Puppet
-
-Documented here are a number of methods Ship provides to facilitating distributing assets to your on-prem customers.
-
-- Inline in your application spec
-- Mirrored from public github repos
-- Proxied from to private github repos
-- Proxied from to private docker registries
-
-We're always interested to hear more about how you're deploying your application internally, if there's an asset delivery method you'd like to see, drop us a line at support@replicated.com or https://help.replicated.com/community.
-
-`;
-
+import { ASSETS_INDEX_DOC, CONFIG_INDEX_DOC, LIFECYCLE_INDEX_DOC } from "./static";
 
 export const name = "markdown-assets";
 export const describe = "Build markdown for examples in top-level assets elements";
@@ -72,6 +40,11 @@ function maybeRenderParameters(required: any[], typeOf) {
 function parseParameters(specTypes: any, specType) {
   const required = [] as any[];
   const optional = [] as any[];
+  const props = _.get(specTypes, `${specType}.properties`);
+  if (!props) {
+    return { required, optional };
+  }
+
   for (const field of Object.keys(specTypes[specType].properties)) {
     let description = specTypes[specType].properties[field].description;
     if (description) {
@@ -89,23 +62,24 @@ function parseParameters(specTypes: any, specType) {
   return {required, optional};
 }
 
-function maybeRenderExamples(specTypes: any, specType) {
+function maybeRenderExamples(specTypes: any, specType, subgroup: string) {
   let doc = "";
   if (specTypes[specType].examples) {
     for (const example of specTypes[specType].examples) {
+      console.log("EXAMPLE", subgroup, specType);
       doc += `
 ${"```yaml"}
-${yaml.safeDump({specs: [{[specType]: example}]})}${"```"}
+${yaml.safeDump({[subgroup]: {v1: [{[specType]: example}]}})}${"```"}
 `;
     }
   }
   return doc;
 }
 
-function writeHeader(specTypes: any, specType) {
+function writeHeader(specTypes: any, specType, subgroup: string) {
   return `---
 categories:
-- support-bundle-yaml-specs
+- ship-${subgroup}
 date: 2018-01-17T23:51:55Z
 description: ${specTypes[specType].description || ""}
 index: docs
@@ -113,6 +87,8 @@ title: ${specType}
 weight: "100"
 gradient: "purpleToPink"
 ---
+
+[Assets](/api/ship-assets/assets) | [Config](/api/ship-config/config) | [Lifecycle](/api/ship-lifecycle/lifecycle) 
 
 ## ${specType}
 
@@ -125,27 +101,33 @@ export const handler = (argv) => {
   const schema = JSON.parse(fs.readFileSync(argv.infile).toString());
 
 
-  fs.writeFileSync(`${argv.output}/assets.md`, INDEX_DOC);
+  fs.writeFileSync(`assets/assets.md`, ASSETS_INDEX_DOC);
+  fs.writeFileSync(`lifecycle/lifecycle.md`, LIFECYCLE_INDEX_DOC);
+  fs.writeFileSync(`config/config.md`, CONFIG_INDEX_DOC);
 
-  const specTypes = schema.properties.assets.properties.v1.items.properties;
-  for (const specType of Object.keys(specTypes)) {
-    console.log(`PROPERTY ${specType}`);
-    const cleanProperty = specType.replace(/\./g, "-");
+  for (let subgroup of ["assets", "lifecycle"]) {
+    const specTypes = _.get(schema, `properties[${subgroup}].properties.v1.items.properties`);
+    if (!specTypes) {
+      continue;
+    }
 
-    let doc = "";
-    doc += writeHeader(specTypes, specType);
-    doc += maybeRenderExamples(specTypes, specType);
+    for (const specType of Object.keys(specTypes)) {
+      console.log(`PROPERTY ${specType}`);
+      const cleanProperty = specType.replace(/\./g, "-");
 
-    const {required, optional} = parseParameters(specTypes, specType);
-    doc += maybeRenderParameters(required, `Required`);
-    doc += maybeRenderParameters(optional, `Optional`);
+      let doc = "";
+      doc += writeHeader(specTypes, specType, subgroup);
+      doc += maybeRenderExamples(specTypes, specType, subgroup);
 
-    doc += `
+      const {required, optional} = parseParameters(specTypes, specType);
+      doc += maybeRenderParameters(required, `Required`);
+      doc += maybeRenderParameters(optional, `Optional`);
+
+      doc += `
     
     `;
 
-
-
-    fs.writeFileSync(`${argv.output}/${cleanProperty}.md`, doc);
+      fs.writeFileSync(`${subgroup}/${cleanProperty}.md`, doc);
+    }
   }
 };
