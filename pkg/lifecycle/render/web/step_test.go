b45f1c7288458ca05d106653dfde099990cee84b
Kevin Herro <kevin@replicated.com>
2018-06-18 09:57:41 -0700

    refactor tests

--- pkg/lifecycle/render/web/step_test.go
+++ pkg/lifecycle/render/web/step_test.go
@@ -6,32 +6,18 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-type TestPullWeb struct {
+type TestWebAsset struct {
 	Name        string
-	URL         string
-	Method      string
-	Body        string
-	Headers     map[string][]string
-	ExpectedErr bool
+	Asset       *Built
+	ExpectedErr error
 }
 
-type TestParseRequest struct {
-	Name        string
-	URL         string
-	Method      string
-	Body        string
-	ExpectedErr bool
-}
-
-func TestPullHelper(t *testing.T) {
-	tests := []TestPullWeb{
+func TestWebStep(t *testing.T) {
+	tests := []TestWebAsset{
 		{
 			Name:        "empty",
-			URL:         "",
-			Method:      "",
-			Body:        "",
-			Headers:     map[string][]string{},
-			ExpectedErr: true,
+			Asset:       nil,
+			ExpectedErr: nil,
 		},
 	}
 
@@ -39,38 +25,6 @@ func TestPullHelper(t *testing.T) {
 		t.Run(test.Name, func(t *testing.T) {
 			req := require.New(t)
 
-			_, err := pullWebAsset(test.URL, test.Method, test.Body, test.Headers)
-			if test.ExpectedErr {
-				req.Error(err)
-			} else {
-				req.NoError(err)
-			}
-		})
-	}
-}
-
-func TestParseWebRequest(t *testing.T) {
-	tests := []TestParseRequest{
-		{
-			Name:        "empty",
-			URL:         "",
-			Method:      "",
-			Body:        "",
-			ExpectedErr: true,
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.Name, func(t *testing.T) {
-			req := require.New(t)
-
-			_, err := parseRequest(test.URL, test.Method, test.Body)
-
-			if test.ExpectedErr {
-				req.Error(err)
-			} else {
-				req.NoError(err)
-			}
 		})
 	}
 }
