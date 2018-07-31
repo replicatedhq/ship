e0f974567cb03af3d4a9dfdd75643d5801d54f61
Kevin Herro <kevin@replicated.com>
2018-07-31 07:03:03 -0700

    headed flag

--- pkg/cli/update.go
+++ pkg/cli/update.go
@@ -29,9 +29,8 @@ func Update() *cobra.Command {
 		},
 	}
 
-	cmd.Flags().String("file", "", "File path to helm chart")
+	cmd.Flags().String("headed", "", "Run in headed mode")
 
-	cmd.Flags().String("raw", "", "File path to already rendered kubernetes YAML. Intended for use with non-helm K8s YAML or with a helm chart that has already been templated.")
 	viper.BindPFlags(cmd.Flags())
 	viper.BindPFlags(cmd.PersistentFlags())
 	viper.AutomaticEnv()
