3a61577d8ed425aacbb0ae7f1d36e7bc98b29c8c
Edward Bramanti <edward@bramanti.org>
2018-08-01 17:25:25 -0700

    Reorganize filepaths

--- pkg/constants/filepaths.go
+++ pkg/constants/filepaths.go
@@ -3,11 +3,11 @@ package constants
 // InstallerPrefixPath is the path prefix of installed assets
 const InstallerPrefixPath = "installer"
 
-// OverlaysPrefixPath is the path prefix of overlays
-const OverlaysPrefixPath = "overlays/ship"
-
 // ShipPath is the default folder path of Ship configuration
 const ShipPath = ".ship"
+
+// OverlaysPrefixPath is the path prefix of overlays
+const OverlaysPrefixPath = "overlays/ship"
 
 // StatePath is the default state file path
 const StatePath = ".ship/state.json"
@@ -15,5 +15,5 @@ const StatePath = ".ship/state.json"
 // KustomizeHelmPath is the path used to store helm chart contents
 const KustomizeHelmPath = "chart"
 
-// TempHelmValuesPath is the path used to store the updated values.yaml
-const TempHelmValuesPath = "chart/tmp"
+// TempHelmValuesPath is the folder path used to store the updated values.yaml
+const TempHelmValuesPath = "tmp"
