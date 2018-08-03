31110334f2066be0d410983d93a723c7aff85d89
Dex <dexter.i.horthy@gmail.com>
2018-08-03 14:23:23 -0700

    split up daemon into v1/v2 routes
    What I Did
    ------------
    
    split up daemon into v1/v2 routes
    
    This "rework-things to support regular nav", is turning out to be a huge project,
    so I'm going to copy stuff over from that other branch by hand, but it has a ton of
    breaking changes that I want to try to avoid.
    
    How I Did it
    ------------
    
    add `V1Routes` and `V2Routes` struct, daemon inherits the routes from these. Someday maybe we'll be able to
    ditch V1 altogether, but I want to try and keep these PRs small and this seems a good way to do it.
    
    How to verify it
    ------------
    
    Should be no visible changes here, except the `lifecycle` route at `/api/v2/lifecycle`
    
    :rowboat:

--- pkg/lifecycle/daemon/constructors.go
+++ pkg/lifecycle/daemon/constructors.go
@@ -26,22 +26,46 @@ func NewHeadlessDaemon(
 }
 
 func NewHeadedDaemon(
+	logger log.Logger,
+	v *viper.Viper,
+	webUIFactory WebUIBuilder,
+	v1Router *V1Routes,
+	v2Router *V2Routes,
+) *ShipDaemon {
+	return &ShipDaemon{
+		Logger:       log.With(logger, "struct", "daemon"),
+		WebUIFactory: webUIFactory,
+		Viper:        v,
+		exitChan:     make(chan error),
+		V1Routes:     v1Router,
+		V2Routes:     v2Router,
+	}
+
+}
+
+func NewV2Router(
+	logger log.Logger,
+) *V2Routes {
+	return &V2Routes{
+		Logger: logger,
+	}
+}
+
+func NewV1Router(
 	v *viper.Viper,
 	renderer *resolve.APIConfigRenderer,
 	stateManager state.Manager,
 	logger log.Logger,
 	ui cli.Ui,
 	fs afero.Afero,
-	webUIFactory WebUIBuilder,
 	treeLoader filetree.Loader,
-) *ShipDaemon {
-	return &ShipDaemon{
-		Logger:             log.With(logger, "struct", "daemon"),
+) *V1Routes {
+	return &V1Routes{
+		Logger:             log.With(logger, "routes", "v1"),
 		Fs:                 fs,
 		UI:                 ui,
 		StateManager:       stateManager,
 		Viper:              v,
-		WebUIFactory:       webUIFactory,
 		TreeLoader:         treeLoader,
 		ConfigSaved:        make(chan interface{}, 1),
 		MessageConfirmed:   make(chan string, 1),
@@ -49,7 +73,6 @@ func NewHeadedDaemon(
 		KustomizeSaved:     make(chan interface{}, 1),
 		ConfigRenderer:     renderer,
 		OpenWebConsole:     tryOpenWebConsole,
-		exitChan:           make(chan error),
 	}
 
 }
