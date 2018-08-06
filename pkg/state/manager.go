64fd4c8f7b837be59f4eae5f91204c8d4ba895c7
Kevin Herro <kevin@replicated.com>
2018-08-06 10:57:34 -0700

    persist chart URL and gitignore

--- pkg/state/manager.go
+++ pkg/state/manager.go
@@ -25,6 +25,7 @@ type Manager interface {
 	TryLoad() (State, error)
 	RemoveStateFile() error
 	SaveKustomize(kustomize *Kustomize) error
+	SerializeChartURL(URL string) error
 }
 
 var _ Manager = &MManager{}
@@ -46,6 +47,16 @@ func NewManager(
 		FS:     fs,
 		V:      v,
 	}
+}
+
+// SerializeChartURL is used by `ship init` to serialize a state file with ChartURL to disk
+func (s *MManager) SerializeChartURL(URL string) error {
+	debug := level.Debug(log.With(s.Logger, "method", "SerializeChartURL"))
+
+	debug.Log("event", "generateChartURLState")
+	toSerialize := VersionedState{V1: &V1{ChartURL: URL}}
+
+	return s.serializeAndWriteState(toSerialize)
 }
 
 // SerializeHelmValues takes user input helm values and serializes a state file to disk
