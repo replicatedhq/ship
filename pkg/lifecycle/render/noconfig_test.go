c46372c6cf240ac83a62b83c4635e8de7cc3831e
Edward Bramanti <edward@bramanti.org>
2018-08-13 11:04:00 -0700

    Fix noconfig

--- pkg/lifecycle/render/noconfig_test.go
+++ pkg/lifecycle/render/noconfig_test.go
@@ -5,21 +5,24 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/replicatedhq/ship/pkg/constants"
+
+	"time"
+
 	_ "github.com/replicatedhq/ship/pkg/lifecycle/render/test-cases"
-		"time"
 
 	"github.com/go-kit/kit/log"
 	"github.com/golang/mock/gomock"
 	"github.com/replicatedhq/ship/pkg/api"
 	"github.com/replicatedhq/ship/pkg/lifecycle/daemon/daemontypes"
 	"github.com/replicatedhq/ship/pkg/lifecycle/render/planner"
+	"github.com/replicatedhq/ship/pkg/state"
 	mockdaemon "github.com/replicatedhq/ship/pkg/test-mocks/daemon"
 	mockplanner "github.com/replicatedhq/ship/pkg/test-mocks/planner"
+	state2 "github.com/replicatedhq/ship/pkg/test-mocks/state"
 	"github.com/replicatedhq/ship/pkg/test-mocks/ui"
 	"github.com/spf13/afero"
-		"github.com/stretchr/testify/assert"
-	state2 "github.com/replicatedhq/ship/pkg/test-mocks/state"
-	"github.com/replicatedhq/ship/pkg/state"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestRenderNoConfig(t *testing.T) {
@@ -51,7 +54,6 @@ func TestRenderNoConfig(t *testing.T) {
 			prog = mockDaemon.EXPECT().SetStepName(ctx, daemontypes.StepNameConfirm).After(prog)
 			mockDaemon.EXPECT().ClearProgress().After(prog)
 
-
 			renderer.StatusReceiver = mockDaemon
 
 			release := &api.Release{Spec: test.Spec}
@@ -62,7 +64,7 @@ func TestRenderNoConfig(t *testing.T) {
 				mockState.EXPECT().TryLoad().Return(state.V0(test.ViperConfig), nil)
 
 				p.EXPECT().
-					Build(test.Spec.Assets.V1, test.Spec.Config.V1, gomock.Any(), test.ViperConfig).
+					Build(constants.InstallerPrefixPath, test.Spec.Assets.V1, test.Spec.Config.V1, gomock.Any(), test.ViperConfig).
 					Return(planner.Plan{}, nil)
 
 				p.EXPECT().
