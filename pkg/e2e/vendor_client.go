55372aecb4b848225743097373aeb4f2d15361de
Dex <dexter.i.horthy@gmail.com>
2018-08-15 13:26:32 -0700

    integration tests ensure release exists
    What I Did
    ------------
    
    Add new integration tests,
    
    How I Did it
    ------------
    
    - Add new integration test folder for `ship init replicated.app?customer_id=...&installation_id=...`,
    - Copy over 1 test, it still fails right now
    - e2e/vendor_client.go updates to support ensuring releases exist
    - have the replicated.app tests try to create the release from `input/.ship/release.yml` in graphql-api
    
    How to verify it
    ------------
    
    ginkgo integration/init__replicated.app
    
    Description for the Changelog
    ------------
    
    None
    
    :ship:

--- pkg/e2e/vendor_client.go
+++ pkg/e2e/vendor_client.go
@@ -10,7 +10,7 @@ import (
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
-	multierror "github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/go-multierror"
 	"github.com/pkg/errors"
 )
 
@@ -35,8 +35,8 @@ type GraphQLError struct {
 	Code      string                   `json:"code"`
 }
 
-// GraphQLResponse is the top-level response object from the graphql server
-type GraphQLResponse struct {
+// GraphQLResponsePromoteRelease is the top-level response object from the graphql server
+type GraphQLResponsePromoteRelease struct {
 	Data   *ShipReleaseResult `json:"data,omitempty"`
 	Errors []GraphQLError     `json:"errors,omitempty"`
 }
@@ -45,13 +45,97 @@ type ShipReleaseResult struct {
 	PromoteResult map[string]interface{} `json:"promoteRelease"`
 }
 
+// GraphQLResponsePromoteRelease is the top-level response object from the graphql server
+type GraphQLResponseCreateChannel struct {
+	Data   *ShipCreateChannelResult `json:"data,omitempty"`
+	Errors []GraphQLError           `json:"errors,omitempty"`
+}
+
+type ShipCustomer struct {
+	InstallationID string `json:"installationId"`
+	ID             string `json:"id"`
+}
+
+type ShipChannel struct {
+	Name      string         `json:"name"`
+	ID        string         `json:"id"`
+	Customers []ShipCustomer `json:"customers"`
+}
+
+type ShipCreateChannelResult struct {
+	CreateChannel ShipChannel `json:"createChannel"`
+}
+
+type ShipAssignCustomerResult struct {
+	AssignCustomerToChannel ShipChannel `json:"assignCustomerToChannel"`
+}
+
+type ShipChannelListResult struct {
+	SearchChannels []ShipChannel `json:"searchChannels"`
+}
+
+type GraphQLResponseListChannel struct {
+	Data   *ShipChannelListResult `json:"data,omitempty"`
+	Errors []GraphQLError         `json:"errors,omitempty"`
+}
+
+type GraphQLResponseAssignCustomer struct {
+	Data   *ShipAssignCustomerResult `json:"data,omitempty"`
+	Errors []GraphQLError            `json:"errors,omitempty"`
+}
+
+func (r GraphQLResponseListChannel) GraphQLError() []GraphQLError {
+	return r.Errors
+}
+func (r GraphQLResponseCreateChannel) GraphQLError() []GraphQLError {
+	return r.Errors
+}
+func (r GraphQLResponsePromoteRelease) GraphQLError() []GraphQLError {
+	return r.Errors
+}
+
+func (r GraphQLResponseAssignCustomer) GraphQLError() []GraphQLError {
+	return r.Errors
+}
+
+type Errer interface {
+	GraphQLError() []GraphQLError
+}
+
+func (c *GraphQLClient) GetOrCreateChannel(name string) (*ShipChannel, error) {
+	requestObj := GraphQLRequest{
+		Query: `
+query($channelName: String!) {
+  searchChannels(channelName: $channelName) {
+    id name
+  }
+}`,
+		Variables: map[string]string{"channelName": name},
+	}
+	response := GraphQLResponseListChannel{}
+	err := c.executeRequest(requestObj, &response)
+	if err != nil {
+		return nil, errors.Wrapf(err, "execute request")
+	}
+
+	if err := c.checkErrors(response); err != nil {
+		return nil, err
+	}
+
+	if response.Data != nil && len(response.Data.SearchChannels) != 0 {
+		return &response.Data.SearchChannels[0], nil
+	}
+
+	channel, err := c.CreateChannel(name)
+	return channel, errors.Wrap(err, "create channel")
+}
+
 func (c *GraphQLClient) PromoteRelease(
 	spec,
 	channelID,
 	semver,
 	releaseNotes string,
 ) (*ShipReleaseResult, error) {
-	debug := log.With(level.Debug(c.Logger), "type", "graphQLClient", "semver", semver)
 
 	requestObj := GraphQLRequest{
 		Query: `
@@ -71,53 +155,127 @@ mutation($channelId: ID!, $semver: String!, $spec: String!, $releaseNotes: Strin
 			"releaseNotes": releaseNotes,
 		},
 	}
-
-	body, err := json.Marshal(requestObj)
+	response := GraphQLResponsePromoteRelease{}
+	err := c.executeRequest(requestObj, &response)
 	if err != nil {
-		return nil, errors.Wrap(err, "marshal body")
+		return nil, errors.Wrapf(err, "execute request")
 	}
-
-	bodyReader := bytes.NewReader(body)
-
-	req, err := http.NewRequest("POST", c.GQLServer.String(), bodyReader)
-	if err != nil {
-		return nil, errors.Wrap(err, "marshal body")
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", c.Token)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
-	if err != nil {
-		return nil, errors.Wrap(err, "marshal body")
-	}
-	if resp == nil {
-		return nil, errors.New("nil response from gql")
-	}
-	if resp.Body == nil {
-		return nil, errors.New("nil response.Body from gql")
-	}
-
-	responseBody, err := ioutil.ReadAll(resp.Body)
-	debug.Log("body", responseBody)
-	if err != nil {
-		return nil, errors.Wrap(err, "marshal body")
-	}
-
-	response := GraphQLResponse{}
-	if err := json.Unmarshal(responseBody, &response); err != nil {
-
-	}
-
-	if response.Errors != nil && len(response.Errors) > 0 {
-		var multiErr *multierror.Error
-		for _, err := range response.Errors {
-			multiErr = multierror.Append(multiErr, fmt.Errorf("%s: %s", err.Code, err.Message))
-
-		}
-		return nil, multiErr.ErrorOrNil()
+	if err := c.checkErrors(response); err != nil {
+		return nil, err
 	}
 
 	return response.Data, nil
+}
+
+func (c *GraphQLClient) CreateChannel(name string) (*ShipChannel, error) {
+	requestObj := GraphQLRequest{
+		Query: `
+mutation($channelName: String!) {
+  createChannel(channelName: $channelName) {
+    id
+    name
+  }
+}`,
+		Variables: map[string]string{
+			"channelName": name,
+		},
+	}
+	response := GraphQLResponseCreateChannel{}
+	err := c.executeRequest(requestObj, &response)
+	if err != nil {
+		return nil, errors.Wrapf(err, "execute request")
+	}
+	if err := c.checkErrors(response); err != nil {
+		return nil, err
+	}
+
+	return &response.Data.CreateChannel, nil
+}
+
+func (c *GraphQLClient) EnsureCustomerOnChannel(customerId string, channelId string) (string, error) {
+	requestObj := GraphQLRequest{
+		Query: `
+mutation($customerId: ID!, $channelId: ID!) {
+  assignCustomerToChannel(customerId: $customerId, channelId: $channelId) {
+    id
+    name
+	customers {
+      id
+      installationId
+    }
+  }
+}`,
+		Variables: map[string]string{
+			"customerId": customerId,
+			"channelId":  channelId,
+		},
+	}
+	response := GraphQLResponseAssignCustomer{}
+	err := c.executeRequest(requestObj, &response)
+	if err != nil {
+		return "", errors.Wrapf(err, "execute request")
+	}
+	if err := c.checkErrors(response); err != nil {
+		return "", err
+	}
+
+	for _, customer := range response.Data.AssignCustomerToChannel.Customers {
+		if customer.ID == customerId {
+			return customer.InstallationID, nil
+		}
+
+	}
+	return "", errors.Errorf("no matching customers returned when assigning customer %s to channel %s", customerId, channelId)
+
+}
+
+func (c *GraphQLClient) executeRequest(
+	requestObj GraphQLRequest,
+	deserializeTarget interface{},
+) error {
+	debug := log.With(level.Debug(c.Logger), "type", "graphQLClient", "semver")
+	body, err := json.Marshal(requestObj)
+	if err != nil {
+		return errors.Wrap(err, "marshal body")
+	}
+	bodyReader := bytes.NewReader(body)
+	req, err := http.NewRequest("POST", c.GQLServer.String(), bodyReader)
+	if err != nil {
+		return errors.Wrap(err, "marshal body")
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", c.Token)
+	client := http.DefaultClient
+	resp, err := client.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "marshal body")
+	}
+	if resp == nil {
+		return errors.New("nil response from gql")
+	}
+	if resp.Body == nil {
+		return errors.New("nil response.Body from gql")
+	}
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	debug.Log("body", responseBody)
+	if err != nil {
+		return errors.Wrap(err, "marshal body")
+	}
+	if err := json.Unmarshal(responseBody, deserializeTarget); err != nil {
+		return errors.Wrap(err, "unmarshal response")
+	}
+
+	return nil
+}
+
+func (c *GraphQLClient) checkErrors(errer Errer) error {
+	if errer.GraphQLError() != nil && len(errer.GraphQLError()) > 0 {
+		var multiErr *multierror.Error
+		for _, err := range errer.GraphQLError() {
+			multiErr = multierror.Append(multiErr, fmt.Errorf("%s: %s", err.Code, err.Message))
+
+		}
+		return multiErr.ErrorOrNil()
+	}
+	return nil
 }
