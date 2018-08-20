375728145ea2b44a311ba3219dddee55bf899654
Robert Oh <robert.is.oh@gmail.com>
2018-08-20 12:59:09 -0700

    address comments

--- pkg/util/filesystem.go
+++ pkg/util/filesystem.go
@@ -1,7 +1,6 @@
 package util
 
 import (
-	"fmt"
 	"path/filepath"
 
 	"github.com/pkg/errors"
@@ -12,30 +11,31 @@ import (
 // TODO make this work like the description says
 func FindOnlySubdir(dir string, fs afero.Afero) (string, error) {
 	subDirExists := false
-	firstSubDirIndex := 0
 
 	files, err := fs.ReadDir(dir)
 	if err != nil {
 		return "", errors.Wrap(err, "failed to read dir")
 	}
 
+	subDir := files[0]
+
 	if len(files) == 0 {
-		return "", errors.New(fmt.Sprintf("no files found in %s", dir))
+		return "", errors.Errorf("no files found in %s", dir)
 	}
 
-	for idx, file := range files {
+	for _, file := range files {
 		if file.IsDir() {
 			if !subDirExists {
 				subDirExists = true
-				firstSubDirIndex = idx
+				subDir = file
 			} else {
-				return "", errors.New(fmt.Sprintf("multiple subdirs found in %s", dir))
+				return "", errors.Errorf("multiple subdirs found in %s", dir)
 			}
 		}
 	}
 
 	if subDirExists {
-		return filepath.Join(dir, files[firstSubDirIndex].Name()), nil
+		return filepath.Join(dir, subDir.Name()), nil
 	}
 
 	return "", errors.New("unable to find a subdirectory")
