c72e868e0db3c5a196f5b01364507c3824ec08b1
Edward Bramanti <edward@bramanti.org>
2018-08-29 16:41:56 -0700

    Move go-get commands to build deps script

--- hack/get_build_deps.sh
+++ hack/get_build_deps.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -v
-go get -u github.com/golang/lint/golint
+
 go get golang.org/x/tools/cmd/goimports
+go get -u github.com/golang/lint/golint
+go get github.com/golang/mock/gomock
+go install github.com/golang/mock/mockgen
 go get github.com/elazarl/go-bindata-assetfs/...
 go get -u github.com/jteeuwen/go-bindata/...
