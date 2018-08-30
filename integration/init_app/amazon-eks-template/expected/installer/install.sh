8e6b0dfbe5e4a9855b4f27b68df956b71f8f07a5
Andrew Lavery <laverya@umich.edu>
2018-08-30 12:02:35 -0700

    update amazon-eks-template init_app test to include template functions

--- integration/init_app/amazon-eks-template/expected/installer/install.sh
+++ integration/init_app/amazon-eks-template/expected/installer/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "run:"
 echo "terraform apply -f new/new_vpc.tf"
-echo "kubectl apply -f kube.yaml --kubeconfig new/kubeconfig_new-vpc-cluster"
+echo "kubectl apply -f kube.yaml --kubeconfig new/kubeconfig_cluster-name-template"
