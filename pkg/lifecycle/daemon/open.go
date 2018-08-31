b9e884c99e45b003522883ebaac9b6533b508a9b
Robert Oh <robert.is.oh@gmail.com>
2018-08-31 09:07:56 -0700

    fix

--- pkg/lifecycle/daemon/open.go
+++ pkg/lifecycle/daemon/open.go
@@ -9,24 +9,24 @@ import (
 	"github.com/skratchdot/open-golang/open"
 )
 
-type opener func(cli.Ui, string) error
+type opener func(cli.Ui, string, bool) error
 
-func tryOpenWebConsole(ui cli.Ui, url string) error {
-	openBrowser, err := ui.Ask("Open browser to continue? (Y/n)")
-	if err != nil {
-		return err
+func tryOpenWebConsole(ui cli.Ui, url string, autoOpen bool) error {
+	if !autoOpen {
+		openBrowser, err := ui.Ask("Open browser to continue? (Y/n)")
+		if err != nil {
+			return err
+		}
+
+		openBrowser = strings.ToLower(strings.Trim(openBrowser, " \r\n"))
+		if strings.Compare(openBrowser, "n") == 0 {
+			ui.Info(fmt.Sprintf(
+				"\nPlease visit the following URL in your browser to continue the installation\n\n        %s\n\n ",
+				url,
+			))
+		}
 	}
 
-	openBrowser = strings.ToLower(strings.Trim(openBrowser, " \r\n"))
-	if strings.Compare(openBrowser, "n") == 0 {
-		ui.Info(fmt.Sprintf(
-			"\nPlease visit the following URL in your browser to continue the installation\n\n        %s\n\n ",
-			url,
-		))
-	} else {
-		ui.Info("\n       Opening console at " + url + " ...")
-		err := open.Start(url)
-		return err
-	}
-	return nil
+	ui.Info("\n       Opening console at " + url + " ...")
+	return open.Start(url)
 }
