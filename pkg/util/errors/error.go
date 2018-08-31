e3bea664370b4b53993f555b7f69d442b7bc7d01
Robert Oh <robert.is.oh@gmail.com>
2018-08-31 15:19:27 -0700

    add a comment

--- pkg/util/errors/error.go
+++ pkg/util/errors/error.go
@@ -1,5 +1,7 @@
 package errors
 
+// FetchFilesError is used by implementations of FileFetcher to represent an error
+// fetching the upstream resource
 type FetchFilesError struct {
 	Message string
 }
