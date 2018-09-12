cbae75f5a99e16f503a005f7fa9238d3730e0cb4
Edward Bramanti <edward@bramanti.org>
2018-09-12 16:46:06 -0700

    Initial react-scripts boilerplate

---
+++ web/ship-app/src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { App } from "./App";
+import "./index.scss";
+import registerServiceWorker from "./registerServiceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root") as HTMLElement);
+registerServiceWorker();
