9772fc2ac821b48381b8a40cc8e5be93ab85aa38
Edward Bramanti <edward@bramanti.org>
2018-09-14 12:41:42 -0700

    Reorganize web file structure - Remove legacy app - app/ => React app incorporated in Ship binary - init/ => React component that contains Ship Init app

--- web/app/env/shipDev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  ENVIRONMENT: "shipDev",
-  API_ENDPOINT: "http://localhost:8800/api/v1",
-  WEBPACK_SCRIPTS: [
-    "https://unpkg.com/react@16/umd/react.production.min.js",
-    "https://unpkg.com/react-dom@16/umd/react-dom.production.min.js"
-  ]
-};
--- web/app/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Root from "./Root";
-import { configStore } from "./redux";
-
-configStore().then(() => {
-  ReactDOM.render((<Root/>), document.getElementById("root"));
-});
