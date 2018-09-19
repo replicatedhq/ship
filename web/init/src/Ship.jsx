1a2df1a88a666f27aed0932f7f7a7e2847a5ad8e
Sloane Smith <s10dimensional@gmail.com>
2018-09-19 13:27:08 -0700

    Scope body, reset to ship-init class, scope all other css to #ship-init-component

--- web/init/src/Ship.jsx
+++ web/init/src/Ship.jsx
@@ -6,16 +6,30 @@ import { configureStore } from "./redux";
 import PropTypes from "prop-types";
 
 import "./scss/index.scss";
+const bodyClass = "ship-init";
 
-export const Ship = ({ apiEndpoint }) => (
-  <div id="ship-init-component">
-    <Provider store={configureStore(apiEndpoint)}>
-      <AppWrapper>
-        <RouteDecider />
-      </AppWrapper>
-    </Provider>
-  </div>
-);
+export class Ship extends React.Component {
+  componentDidMount() {
+    document.body.classList.add(bodyClass);
+  }
+  componentWillUnmount() {
+    document.body.classList.remove(bodyClass);
+  }
+
+  render() {
+    const { apiEndpoint } = this.props;
+
+    return (
+      <div id="ship-init-component">
+        <Provider store={configureStore(apiEndpoint)}>
+          <AppWrapper>
+            <RouteDecider />
+          </AppWrapper>
+        </Provider>
+      </div>
+    )
+  }
+}
 
 Ship.propTypes = {
   apiEndpoint: PropTypes.string.isRequired,
