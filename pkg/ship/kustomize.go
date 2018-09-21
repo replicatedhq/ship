cf15366b380c81ab7bf9c01f478bfd6f100684b7
Dex <dexter.i.horthy@gmail.com>
2018-09-21 09:37:07 -0700

    typo

--- pkg/ship/kustomize.go
+++ pkg/ship/kustomize.go
@@ -50,7 +50,7 @@ func (s *Ship) Init(ctx context.Context) error {
 	existingState, _ := s.State.TryLoad()
 	if existingState != nil {
 		if !existingState.IsEmpty() {
-			s.UI.Output(fmt.Sprintf("exsiting state = %#v\n", existingState))
+			s.UI.Output(fmt.Sprintf("existing state = %#v\n", existingState))
 			debug.Log("event", "existing.state")
 
 			if s.Viper.GetString("state-from") != "file" {
