09fe157aa32c3d80bddb5a253d50a63756ca51b3
Sloane Smith <s10dimensional@gmail.com>
2018-10-22 14:55:40 -0700

    TEMP, removed intro step

--- pkg/specs/chart.go
+++ pkg/specs/chart.go
@@ -41,17 +41,9 @@ func (r *Resolver) DefaultHelmRelease(chartPath string) api.Spec {
 		Lifecycle: api.Lifecycle{
 			V1: []api.Step{
 				{
-					HelmIntro: &api.HelmIntro{
-						StepShared: api.StepShared{
-							ID: "intro",
-						},
-					},
-				},
-				{
 					HelmValues: &api.HelmValues{
 						StepShared: api.StepShared{
 							ID:          "values",
-							Requires:    []string{"intro"},
 							Invalidates: []string{"render"},
 						},
 					},
