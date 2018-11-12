35d17969e5ed9a72fb0112f844ab79460d9298af
Robert Oh <robert.is.oh@gmail.com>
2018-11-12 09:41:57 -0800

    don't write bases in excludedBases

--- pkg/lifecycle/kustomize/kustomizer_test.go
+++ pkg/lifecycle/kustomize/kustomizer_test.go
@@ -487,31 +487,35 @@ func TestKustomizer_shouldAddFile(t *testing.T) {
 	k := daemonkustomizer{}
 
 	tests := []struct {
-		name       string
-		targetPath string
-		want       bool
+		name          string
+		targetPath    string
+		want          bool
+		excludedPaths []string
 	}{
-		{name: "empty", targetPath: "", want: false},
-		{name: "no extension", targetPath: "file", want: false},
-		{name: "wrong extension", targetPath: "file.txt", want: false},
-		{name: "yaml file", targetPath: "file.yaml", want: true},
-		{name: "yml file", targetPath: "file.yml", want: true},
-		{name: "kustomization yaml", targetPath: "kustomization.yaml", want: false},
-		{name: "Chart yaml", targetPath: "Chart.yaml", want: false},
-		{name: "values yaml", targetPath: "values.yaml", want: false},
-		{name: "no extension in dir", targetPath: "dir/file", want: false},
-		{name: "wrong extension in dir", targetPath: "dir/file.txt", want: false},
-		{name: "yaml in dir", targetPath: "dir/file.yaml", want: true},
-		{name: "yml in dir", targetPath: "dir/file.yml", want: true},
-		{name: "kustomization yaml in dir", targetPath: "dir/kustomization.yaml", want: false},
-		{name: "Chart yaml in dir", targetPath: "dir/Chart.yaml", want: false},
-		{name: "values yaml in dir", targetPath: "dir/values.yaml", want: false},
+		{name: "empty", targetPath: "", want: false, excludedPaths: []string{}},
+		{name: "no extension", targetPath: "file", want: false, excludedPaths: []string{}},
+		{name: "wrong extension", targetPath: "file.txt", want: false, excludedPaths: []string{}},
+		{name: "yaml file", targetPath: "file.yaml", want: true, excludedPaths: []string{}},
+		{name: "yml file", targetPath: "file.yml", want: true, excludedPaths: []string{}},
+		{name: "kustomization yaml", targetPath: "kustomization.yaml", want: false, excludedPaths: []string{}},
+		{name: "Chart yaml", targetPath: "Chart.yaml", want: false, excludedPaths: []string{}},
+		{name: "values yaml", targetPath: "values.yaml", want: false, excludedPaths: []string{}},
+		{name: "no extension in dir", targetPath: "dir/file", want: false, excludedPaths: []string{}},
+		{name: "wrong extension in dir", targetPath: "dir/file.txt", want: false, excludedPaths: []string{}},
+		{name: "yaml in dir", targetPath: "dir/file.yaml", want: true, excludedPaths: []string{}},
+		{name: "yml in dir", targetPath: "dir/file.yml", want: true, excludedPaths: []string{}},
+		{name: "kustomization yaml in dir", targetPath: "dir/kustomization.yaml", want: false, excludedPaths: []string{}},
+		{name: "Chart yaml in dir", targetPath: "dir/Chart.yaml", want: false, excludedPaths: []string{}},
+		{name: "values yaml in dir", targetPath: "dir/values.yaml", want: false, excludedPaths: []string{}},
+		{name: "path in excluded", targetPath: "deployment.yaml", want: false, excludedPaths: []string{"/deployment.yaml"}},
+		{name: "path not in excluded", targetPath: "service.yaml", want: true, excludedPaths: []string{"/deployment.yaml"}},
+		{name: "similar path in excluded", targetPath: "dir/service.yaml", want: true, excludedPaths: []string{"/service.yaml"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			req := require.New(t)
 
-			got := k.shouldAddFileToBase(tt.targetPath)
+			got := k.shouldAddFileToBase(tt.excludedPaths, tt.targetPath)
 
 			req.Equal(tt.want, got, "expected %t for path %s, got %t", tt.want, tt.targetPath, got)
 		})
