81676656900658677f075a82dd38012c4b2ba4d2
Robert Oh <robert.is.oh@gmail.com>
2018-11-27 13:50:26 -0800

    move splitting multi doc yaml to kustomize

--- pkg/lifecycle/kustomize/pre_kustomize.go
+++ pkg/lifecycle/kustomize/pre_kustomize.go
@@ -22,6 +22,11 @@ type ListK8sYaml struct {
 }
 
 func (l *Kustomizer) PreExecute(ctx context.Context, step api.Step) error {
+	// Split multi doc yaml first as it will be unmarshalled incorrectly in the following steps
+	if err := l.maybeSplitMultidocYaml(ctx, step.Kustomize.Base); err != nil {
+		return errors.Wrap(err, "maybe split multi doc yaml")
+	}
+
 	if err := l.maybeSplitListYaml(ctx, step.Kustomize.Base); err != nil {
 		return errors.Wrap(err, "maybe split list yaml")
 	}
--- pkg/specs/chart.go
+++ pkg/specs/chart.go
@@ -7,7 +7,6 @@ import (
 	"os"
 	"path"
 	"path/filepath"
-	"strings"
 
 	"github.com/go-kit/kit/log"
 	"github.com/go-kit/kit/log/level"
@@ -15,7 +14,6 @@ import (
 	"github.com/replicatedhq/libyaml"
 	"github.com/replicatedhq/ship/pkg/api"
 	"github.com/replicatedhq/ship/pkg/constants"
-	"github.com/replicatedhq/ship/pkg/state"
 	"github.com/replicatedhq/ship/pkg/util"
 	"gopkg.in/yaml.v2"
 )
@@ -333,82 +331,4 @@ func (r *Resolver) calculateContentSHA(root string) (string, error) {
 	}
 
 	return fmt.Sprintf("%x", sha256.Sum256(contents)), nil
-}
-
-// this function is not perfect, and has known limitations. One of these is that it does not account for `\n---\n` in multiline strings.
-func (r *Resolver) maybeSplitMultidocYaml(ctx context.Context, localPath string) error {
-	type outputYaml struct {
-		name     string
-		contents string
-	}
-
-	files, err := r.FS.ReadDir(localPath)
-	if err != nil {
-		return errors.Wrapf(err, "read files in %s", localPath)
-	}
-
-	if len(files) != 1 {
-		// if there's more than one file, we'll assume that it does not need to be split
-		// if there are no files, there's nothing to do
-		return nil
-	}
-
-	file := files[0]
-
-	if file.IsDir() {
-		// if the single file is a directory, obviously we can't split it
-		return nil
-	}
-
-	if filepath.Ext(file.Name()) != ".yaml" && filepath.Ext(file.Name()) != ".yml" {
-		// not yaml, nothing to do
-		return nil
-	}
-
-	inFileBytes, err := r.FS.ReadFile(filepath.Join(localPath, file.Name()))
-	if err != nil {
-		return errors.Wrapf(err, "read %s", filepath.Join(localPath, file.Name()))
-	}
-
-	outputFiles := []outputYaml{}
-	filesStrings := strings.Split(string(inFileBytes), "\n---\n")
-
-	// generate replacement yaml files
-	for idx, fileString := range filesStrings {
-
-		thisOutputFile := outputYaml{contents: fileString}
-
-		thisMetadata := state.MinimalK8sYaml{}
-		_ = yaml.Unmarshal([]byte(fileString), &thisMetadata)
-
-		if thisMetadata.Kind == "" {
-			// not a valid k8s yaml
-			continue
-		}
-
-		fileName := util.GenerateNameFromMetadata(thisMetadata, idx)
-		thisOutputFile.name = fileName
-		outputFiles = append(outputFiles, thisOutputFile)
-	}
-
-	if len(outputFiles) < 2 {
-		// not a multidoc yaml, or at least not a multidoc kubernetes yaml
-		return nil
-	}
-
-	// delete multidoc yaml file
-	err = r.FS.Remove(filepath.Join(localPath, file.Name()))
-	if err != nil {
-		return errors.Wrapf(err, "unable to remove %s", filepath.Join(localPath, file.Name()))
-	}
-
-	// write replacement yaml
-	for _, outputFile := range outputFiles {
-		err = r.FS.WriteFile(filepath.Join(localPath, outputFile.name+".yaml"), []byte(outputFile.contents), os.FileMode(0644))
-		if err != nil {
-			return errors.Wrapf(err, "write %s", outputFile.name)
-		}
-	}
-
-	return nil
 }
