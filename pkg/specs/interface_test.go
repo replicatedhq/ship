8d032244d0c5edd779df9af1db1d900f18279fff
Robert Oh <robert.is.oh@gmail.com>
2018-12-05 18:51:22 -0800

    fix tests

--- pkg/specs/interface_test.go
+++ pkg/specs/interface_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/replicatedhq/ship/pkg/api"
 	replicatedapp2 "github.com/replicatedhq/ship/pkg/specs/replicatedapp"
+	state2 "github.com/replicatedhq/ship/pkg/state"
 	"github.com/replicatedhq/ship/pkg/test-mocks/apptype"
 	"github.com/replicatedhq/ship/pkg/test-mocks/githubclient"
 	"github.com/replicatedhq/ship/pkg/test-mocks/replicatedapp"
@@ -94,6 +95,7 @@ icon: https://kfbr.392/x5.png
 				}, "helm").After(inOrder)
 				inOrder = mockUi.EXPECT().Info("Looking for ship.yaml ...").After(inOrder)
 				inOrder = mockUi.EXPECT().Info("ship.yaml not found in upstream, generating default lifecycle for application ...").After(inOrder)
+				inOrder = mockState.EXPECT().TryLoad().Return(state2.VersionedState{}, nil).After(inOrder)
 				mockState.EXPECT().SerializeReleaseName("i-know-what-the-x5-is").After(inOrder)
 
 			},
@@ -186,6 +188,7 @@ icon: https://kfbr.392/x5.png
 				inOrder = mockState.EXPECT().SerializeContentSHA("abcdef1234567890").After(inOrder)
 				inOrder = mockUi.EXPECT().Info("Looking for ship.yaml ...").After(inOrder)
 				inOrder = mockUi.EXPECT().Info("ship.yaml not found in upstream, generating default lifecycle for application ...").After(inOrder)
+				inOrder = mockState.EXPECT().TryLoad().Return(state2.VersionedState{}, nil).After(inOrder)
 				mockState.EXPECT().SerializeReleaseName("ship").After(inOrder)
 			},
 			expectRelease: &api.Release{
