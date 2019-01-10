20b86db9b5b897642faf97834825b1b953f2a1a3
Robert Oh <robert.is.oh@gmail.com>
2019-01-10 11:21:53 -0800

    error when upstreams dont match

--- pkg/ship/kustomize.go
+++ pkg/ship/kustomize.go
@@ -71,7 +71,10 @@ func (s *Ship) Init(ctx context.Context) error {
 					return err
 				}
 			} else {
-				s.UI.Info(fmt.Sprint("Preserving existing current state"))
+				s.UI.Info(fmt.Sprint("Preserving current state"))
+				if !s.upstreamMatchesExisting(existingState) {
+					return errors.New(fmt.Sprintf("Upstream %s does not match upstream from state %s", s.Viper.GetString("upstream"), existingState.Upstream()))
+				}
 			}
 		}
 	}
@@ -144,4 +147,10 @@ func (s *Ship) fakeKustomizeRawRelease() *api.Release {
 	return &api.Release{
 		Spec: r.DefaultRawRelease(s.KustomizeRaw),
 	}
+}
+
+func (s *Ship) upstreamMatchesExisting(existing state.State) bool {
+	currentUpstream := s.Viper.GetString("upstream")
+	existingUpstream := existing.Upstream()
+	return currentUpstream == existingUpstream
 }
