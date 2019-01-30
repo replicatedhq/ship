5eeb842eefdc4469f8f63727394eecc5f05b9600
Grayson Null <graysonnull@me.com>
2019-01-30 12:08:40 -0800

    [ch15590] save values in state and only update with value from prop if input is not focused

--- web/init/src/components/config_render/ConfigInput.jsx
+++ web/init/src/components/config_render/ConfigInput.jsx
@@ -3,10 +3,32 @@ import ConfigItemTitle from "./ConfigItemTitle";
 
 export default class ConfigInput extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.inputRef = React.createRef();
+    this.state = {
+      inputVal: "",
+      focused: false
+    }
+  }
+
   handleOnChange = (e) => {
     const { handleOnChange, name } = this.props;
+    this.setState({ inputVal: e.target.value });
     if (handleOnChange && typeof handleOnChange === "function") {
       handleOnChange(name, e.target.value);
+    }
+  }
+
+  componentDidUpdate(lastProps) {
+    if (this.props.value !== lastProps.value && !this.state.focused) {
+      this.setState({ inputVal: this.props.value });
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.value) {
+      this.setState({ inputVal: this.props.value });
     }
   }
 
@@ -24,13 +46,16 @@ export default class ConfigInput extends React.Component {
         {this.props.help_text !== "" ? <p className="field-section-help-text u-marginTop--small u-lineHeight--normal">{this.props.help_text}</p> : null}
         <div className="field-input-wrapper u-marginTop--15">
           <input
+            ref={this.inputRef}
             type={this.props.inputType}
             {...this.props.props}
             placeholder={this.props.default}
-            value={this.props.value}
+            value={this.state.inputVal}
             readOnly={this.props.readonly}
             disabled={this.props.readonly}
             onChange={(e) => this.handleOnChange(e)}
+            onFocus={() => this.setState({ focused: true })}
+            onBlur={() => this.setState({ focused: false })}
             className={`${this.props.className || ""} Input ${this.props.readonly ? "readonly" : ""}`} />
         </div>
       </div>
--- web/init/src/components/config_render/ConfigTextarea.jsx
+++ web/init/src/components/config_render/ConfigTextarea.jsx
@@ -3,10 +3,32 @@ import ConfigItemTitle from "./ConfigItemTitle";
 
 export default class ConfigTextarea extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.textareaRef = React.createRef();
+    this.state = {
+      textareaVal: "",
+      focused: false
+    }
+  }
+
   handleOnChange = (e) => {
     const { handleOnChange, name } = this.props;
+    this.setState({ textareaVal: e.target.value });
     if (handleOnChange && typeof handleOnChange === "function") {
       handleOnChange(name, e.target.value);
+    }
+  }
+
+  componentDidUpdate(lastProps) {
+    if (this.props.value !== lastProps.value && !this.state.focused) {
+      this.setState({ textareaVal: this.props.value });
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.value) {
+      this.setState({ textareaVal: this.props.value });
     }
   }
 
@@ -24,12 +46,15 @@ export default class ConfigTextarea extends React.Component {
         {this.props.help_text !== "" ? <p className="field-section-help-text u-marginTop--small u-lineHeight--normal">{this.props.help_text}</p> : null}
         <div className="field-input-wrapper u-marginTop--15">
           <textarea
+            ref={this.textareaRef}
             {...this.props.props}
             placeholder={this.props.default}
-            value={this.props.value}
+            value={this.state.textareaVal}
             readOnly={this.props.readonly}
             disabled={this.props.readonly}
             onChange={(e) => this.handleOnChange(e)}
+            onFocus={() => this.setState({ focused: true })}
+            onBlur={() => this.setState({ focused: false })}
             className={`${this.props.className || ""} Textarea ${this.props.readonly ? "readonly" : ""}`}>
           </textarea>
         </div>
