b8b3ec67a451f8fe6614368d28e9f2b62671a7c1
Grayson Null <graysonnull@me.com>
2019-02-01 14:14:43 -0800

    update css and add default status images

--- web/init/src/components/shared/StepMessage.jsx
+++ web/init/src/components/shared/StepMessage.jsx
@@ -3,18 +3,20 @@ import Markdown from "react-remarkable";
 import RenderActions from "./RenderActions";
 
 const StepMessage = ({ actions, message, handleAction, isLoading }) => (
-  <div className="StepMessage-wrapper flex flex1 flex-column">
-    <div className={`markdown-wrapper flex flex1 flex-column u-overflow--auto  ${message.level || ""}`}>
-      <Markdown
-        options={{
-          html: message.trusted_html,
-          linkTarget: "_blank",
-          linkify: true,
-        }}>
-        {message.contents}
-      </Markdown>
+  <div className="StepMessage-wrapper flex1 flex-column">
+    <div className={`markdown-wrapper flex1 flex-column u-overflow--auto  ${message.level || ""}`}>
+      <div className="mkdwn">
+        <Markdown
+          options={{
+            html: message.trusted_html,
+            linkTarget: "_blank",
+            linkify: true,
+          }}>
+          {message.contents}
+        </Markdown>
+      </div>
     </div>
-    <div className="u-marginTop--30 flex justifyContent--flexEnd">
+    <div className="flex flex-auto justifyContent--flexEnd actions-wrapper u-paddingRight--20 u-paddingLeft--20">
       <RenderActions actions={actions} handleAction={handleAction} isLoading={isLoading} />
     </div>
   </div>
