b39a3066e4dfa71d577105b6c2b6f51033539675
Ben Junya <ben@replicated.com>
2019-05-24 16:16:38 -0700

    [cb20124] - Add error boundary calling onError prop in <Ship> Component

--- web/init/src/ErrorBoundary.jsx
+++ web/init/src/ErrorBoundary.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+/**
+ * Creates an error boundary for any child component rendered inside
+ */
 export default class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
@@ -10,21 +13,25 @@ export default class ErrorBoundary extends React.Component {
     }
   }
 
-  componentDidCatch(error, info) {
-    this.setState({
-      error: error,
-      errorInfo: info,
+  static getDerivedStateFromError(error) {
+    return {
+      error,
       hasError: true
-    });
+    };
+  }
+
+  componentDidCatch() {
     // We can thow an error modal, or some sort of error UI that can be used globally.
     // We should also send this error to bugsnag or some sort of reporting service here.
   }
 
   render() {
+    const { children } = this.props;
+    const { hasError, error } = this.state;
     return (
       <div className="flex flex1">
-        {this.state.hasError ? <div>{this.state.error.toString()}</div> : null}
-        {this.props.children}
+        {hasError && <div>{error.toString()}</div>}
+        {children}
       </div>
     );
   }
