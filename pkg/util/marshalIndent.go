7f97fbaf4f1f51b6f2871b623beaa401baf410a7
Andrew Lavery <laverya@umich.edu>
2019-07-10 12:44:20 -0700

    use laverya/yaml to allow marshalling yaml with unlimited line lengths
    test to ensure that multiline strings are split intelligently when rendered

--- pkg/util/marshalIndent.go
+++ pkg/util/marshalIndent.go
@@ -4,13 +4,14 @@ import (
 	"bytes"
 
 	"github.com/pkg/errors"
-	yaml "gopkg.in/yaml.v3"
+	yaml "gopkg.in/laverya/yaml.v3"
 )
 
 func MarshalIndent(indent int, in interface{}) ([]byte, error) {
 	var buf bytes.Buffer
 	enc := yaml.NewEncoder(&buf)
 	enc.SetIndent(indent)
+	enc.SetLineLength(-1)
 	err := enc.Encode(in)
 	if err != nil {
 		return nil, errors.Wrapf(err, "marshal with indent %d", indent)
