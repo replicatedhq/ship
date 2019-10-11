25c72ebc9e49532bc9ef0459d29f56165b2c60cf
Rui Chen <rchen@meetup.com>
2019-10-11 10:51:57 -0400

    Upgrade golangci-lint to v1.20.0

--- hack/get_lint_deps.sh
+++ hack/get_lint_deps.sh
@@ -3,5 +3,5 @@ set -v
 
 go get -u golang.org/x/tools/cmd/goimports
 go get -u golang.org/x/lint/golint
-go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.17.1
+go get github.com/golangci/golangci-lint/cmd/golangci-lint@v1.20.0
 go get -u github.com/gordonklaus/ineffassign
